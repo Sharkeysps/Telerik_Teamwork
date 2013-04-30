609263e775bc683872056afcb416d66bcd1515d7
Spas Tyurkedzhiev <harda@abv.bg>
2013-04-30 18:49:46 +0300

    Basic splitting of classes.Created classes for printing the game field and the choice of type of explosions but still much left.Game is working

--- Program.cs
+++ Program.cs
@@ -13,85 +13,18 @@ class BattleField
     // Console.WriteLine("test");
     //TODO make a static class with the types of explosions and the switch case 
     //5 types of explosions
-    static readonly int[,] минаЕдно = {{0,0,0,0,0},
-                          {0,1,0,1,0},
-                          {0,0,1,0,0},
-                          {0,1,0,1,0},
-                          {0,0,0,0,0}};
-    static readonly int[,] минаДве =    {{0,0,0,0,0},
-                            {0,1,1,1,0},
-                            {0,1,1,1,0},
-                            {0,1,1,1,0},
-                            {0,0,0,0,0}};
-    static readonly int[,] минаТри =    {{0,0,1,0,0},
-                            {0,1,1,1,0},
-                            {1,1,1,1,1},
-                            {0,1,1,1,0},
-                            {0,0,1,0,0}};
-    static readonly int[,] минаЧетири =    {{0,1,1,1,0},
-                            {1,1,1,1,1},
-                            {1,1,1,1,1},
-                            {1,1,1,1,1},
-                            {0,1,1,1,0}};
-    static readonly int[,] минаПет =    {{1,1,1,1,1},
-                            {1,1,1,1,1},
-                            {1,1,1,1,1},
-                            {1,1,1,1,1},
-                            {1,1,1,1,1}};
-
+   
 
     //test test test
 
 
-    public static void PrintField(int[,] arr, int n)
-    {
-        Console.Write(" ");
-        for (int i = 0; i < n; i++)
-        {
 
-
-
-            Console.Write(" {0}", i);
-        }
-        Console.WriteLine();
-        Console.Write("  ");
-        for (int i = 0; i < n * 2; i++)
-        {
-            
-            
-            
-            Console.Write("-");
-        }
-        Console.WriteLine();
-        for (int i = 0; i < n; i++)
-        {   Console.Write("{0}|", i);
-            for (int j = 0; j < n; j++)
-            {
-                char c;
-                switch (arr[i, j])
-                {
-                    case 0: c = '-'; break;
-                    case -1: c = 'X'; break;
-                    default: c = (char)('0' + arr[i, j]); break;
-                }
-                Console.Write("{0} ", c);
-            }
-            Console.WriteLine();
-        }
-    }
 
     public static int GrymOtQsnoNebe(int[,] arr, int n, int x, int y)
     {
         int[,] expl;
         //TODO make a new class for making the type of explosion
-        switch (arr[x, y]) // zadava ni koi vid bomba imame
-        {
-            case 1: expl = минаЕдно; break;
-            case 2: expl = минаДве; break;
-            case 3: expl = минаТри; break;
-            case 4: expl = минаЧетири; break;
-            default: expl = минаПет; break;
-        }
+        expl=TypesOfExplosionsChoice.ExplosionChoice(arr, x, y);
         //gyrmi bombata
         int counter = 0;
         for (int i = -2; i < 3; i++)
@@ -172,13 +105,14 @@ class BattleField
             }
             arr[x, y] = ProizvolniChisla.Next(1, 6);
         }
-        PrintField(arr, n);
+        PrintGameField.PrintField(arr, n);
+        
         int 爆 = 0;
         while (mineNumber > 0)
         {
             int tmp = TimeToPlay(arr, n);
             mineNumber -= tmp;
-            PrintField(arr, n);
+            PrintGameField.PrintField(arr, n);
             //Console.WriteLine("Mines Blowed this round: {0}",tmp);
             爆++;
         }
---
+++ TypesOfExplosionsChoice.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// Putting the type of explosions and its choice in static class
+/// </summary>
+   static class TypesOfExplosionsChoice
+    {
+        static readonly int[,] минаЕдно = {{0,0,0,0,0},
+                          {0,1,0,1,0},
+                          {0,0,1,0,0},
+                          {0,1,0,1,0},
+                          {0,0,0,0,0}};
+        static readonly int[,] минаДве =    {{0,0,0,0,0},
+                            {0,1,1,1,0},
+                            {0,1,1,1,0},
+                            {0,1,1,1,0},
+                            {0,0,0,0,0}};
+        static readonly int[,] минаТри =    {{0,0,1,0,0},
+                            {0,1,1,1,0},
+                            {1,1,1,1,1},
+                            {0,1,1,1,0},
+                            {0,0,1,0,0}};
+        static readonly int[,] минаЧетири =    {{0,1,1,1,0},
+                            {1,1,1,1,1},
+                            {1,1,1,1,1},
+                            {1,1,1,1,1},
+                            {0,1,1,1,0}};
+        static readonly int[,] минаПет =    {{1,1,1,1,1},
+                            {1,1,1,1,1},
+                            {1,1,1,1,1},
+                            {1,1,1,1,1},
+                            {1,1,1,1,1}};
+        public static int[,] ExplosionChoice(int[,] arr, int x, int y)
+        {
+            switch (arr[x, y]) // zadava ni koi vid bomba imame
+            {
+                case 1: return минаЕдно; break;
+                case 2: return минаДве; break;
+                case 3: return минаТри; break;
+                case 4: return минаЧетири; break;
+                default: return минаПет; break;
+            }
+        }
+    }
+
