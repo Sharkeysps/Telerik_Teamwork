67c8579b01792edfecfdaef7035ddffb6da57f44
Spas Tyurkedzhiev <harda@abv.bg>
2013-05-12 13:31:44 +0300

    Finished the refactoring part.Added some exception handling

--- GameEngine.cs
+++ GameEngine.cs
@@ -6,7 +6,7 @@ public static class GameEngine
     public static void InitiateGame()
     {
         GameFieldGenerator.InputFieldSize();
-
+        
         PrintGameField.PrintField(GameFieldGenerator.GameField, 
             GameFieldGenerator.GameField.GetLength(0));
 
@@ -15,17 +15,19 @@ public static class GameEngine
             GameFieldGenerator.GameField.GetLength(0));
 
     }
-    private static void CheckForVictory(int minesNumber, int[,] gameField, int fieldSize)
+    private static void CheckForVictory(int totalMinesNumber, int[,] gameField, int fieldSize)
     {
         int totalNumberOfMoves = 0;
-        while (minesNumber > 0)
+
+        while (totalMinesNumber > 0)
         {
-            int tmp = GameInput.InputVariables(gameField, fieldSize);
-            minesNumber -= tmp;
+            int blownMinesThisRound = GameInput.InputVariables(gameField, fieldSize);
+            totalMinesNumber -= blownMinesThisRound;
             PrintGameField.PrintField(gameField, fieldSize);
-            //Console.WriteLine("Mines Blowed this round: {0}",tmp);
+            Console.WriteLine("Mines Blowed this round: {0}",blownMinesThisRound);
             totalNumberOfMoves++;
         }
-        Console.WriteLine("Total number of moves：{0}", totalNumberOfMoves);
+
+        Console.WriteLine("Congratulations you won the game in {0} moves", totalNumberOfMoves);
     }
 }
