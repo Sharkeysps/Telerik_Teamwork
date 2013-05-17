a6dc029a98acc156b56f5f88c206e35015911e24
Spas Tyurkedzhiev <harda@abv.bg>
2013-05-17 10:33:00 +0300

    Game Input unit testing completed

--- Mines.Tests/GameInputTests.cs
+++ Mines.Tests/GameInputTests.cs
@@ -14,7 +14,6 @@ namespace Mines.Tests
     public class GameInputTests
     {
         [TestMethod]
-        // [ExpectedException(typeof(NullReferenceException))]
         public void TestUserInputOutsideOfField()
         {
 
@@ -22,11 +21,57 @@ namespace Mines.Tests
             {
                 Console.SetOut(sw);
 
-                using (StringReader sr = new StringReader(string.Format("10{0}5 5{0}", Environment.NewLine)))
+                using (StringReader sr = new StringReader(string.Format("8{0}9 9{0}4 4{0}", Environment.NewLine)))
                 {
                     Console.SetIn(sr);
                     GameBoardGenerator.GetBoardSize();
                     GameInput.ManageUserInput(GameBoardGenerator.GameField);
+                    string output = sw.ToString();
+                    string expectedresult ="Welcome to \"Battle Field\" game. \nEnter battle field size between 1 and 10: Please enter coordinates: Outside of Field Range  Please enter coordinates: ";
+                    Assert.AreEqual(expectedresult.Length, output.Length);
+
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestUserInputIncorrectFormat()
+        {
+
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                using (StringReader sr = new StringReader(string.Format("8{0}ssss{0}4 4{0}", Environment.NewLine)))
+                {
+                    Console.SetIn(sr);
+                    GameBoardGenerator.GetBoardSize();
+                    GameInput.ManageUserInput(GameBoardGenerator.GameField);
+                    string output = sw.ToString();
+                    string expectedresult = "Welcome to \"Battle Field\" game.\r\nEnter battle field size between 1 and 10: Please enter coordinates: Choose x and y coordinates, seperated by white space in correct format [Example:2 5]\r\nPlease enter coordinates: ";
+                    Assert.AreEqual(expectedresult.Length, output.Length);
+
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestUserInputIncorrectFormatLength2()
+        {
+
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                using (StringReader sr = new StringReader(string.Format("8{0}ss{0}4 4{0}", Environment.NewLine)))
+                {
+                    Console.SetIn(sr);
+                    GameBoardGenerator.GetBoardSize();
+                    GameInput.ManageUserInput(GameBoardGenerator.GameField);
+                    string output = sw.ToString();
+                    string expectedresult = "Welcome to \"Battle Field\" game.\r\nEnter battle field size between 1 and 10: Please enter coordinates: Choose x and y coordinates, seperated by white space in correct format [Example:2 5]\r\nPlease enter coordinates: ";
+                    Assert.AreEqual(expectedresult.Length, output.Length);
+
                 }
             }
         }
